4b614a87fc9b6b50353e423745887bcaa981fd0d
Reza Shokri <bioharz3@gmail.com>
2017-06-05 16:10:04 +0200

    play around with lumen

--- public_html/backend/routes/web.php
+++ public_html/backend/routes/web.php
@@ -12,84 +12,20 @@
 */
 
 $app->get('/', function () use ($app) {
-    return $app->version();
+    //return $app->version();
+    return null;
 });
-
-
-$app->get('foo', function () {
-    return 'Hello World';
-});
-
-$app->post('foo', function () {
-    //
-    return "post";
-});
-
-/*
-$app->get('user/{id}', function ($id) {
-    return 'User '.$id;
-});
-*/
-
-/*
-$app->get('profile', [
-    'as' => 'profile', 'uses' => 'UserController@showProfile'
-]);
-*/
-/*
-$app->get('profile', ['as' => 'profile', function () {
-    //
-}]);
-*/
-
-
-
-$app->get('user/{name:[A-Za-z]+}', function ($name) {
-    return "this should be a string: " . gettype($name) . ", the value is: ".$name ;
-});
-
-
-$app->get('user/{name:[1-9]+}', function ($name) {
-    return "this should be a number: " . gettype(intval($name)). ", the value is: ".$name ;
-});
-
-
-$app->get('profile', [
-    'as' => 'profile', 'uses' => 'UserController@showProfile'
-]);
-
-
-/*
-$app->get('user[/{name}]', function ($name = null) {
-    return $name;
-});
-*/
-
-
-/*
-// Generating URLs...
-$url = route('profile');
-
-// Generating Redirects...
-return redirect()->route('profile');
-
-*/
-
-
-$app->get('user/{id}/profile', ['as' => 'profile', function ($id) {
-    return "profile, userid:".$id;
-}]);
-
-$url = route('profile', ['id' => 1]);
-
-
 
 $app->group(['middleware' => 'auth'], function () use ($app) {
+
     $app->get('/', function ()    {
         // Uses Auth Middleware
     });
+
 
     $app->get('user/profile', function () {
         // Uses Auth Middleware
     });
 });
+
+
