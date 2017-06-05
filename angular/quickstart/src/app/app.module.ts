f9a9ff554d4ea1b9b69c889e550e6f967910dc82
Reza Shokri <bioharz3@gmail.com>
2017-06-05 19:40:59 +0200

    routings.. not finished yet

--- angular/quickstart/src/app/app.module.ts
+++ angular/quickstart/src/app/app.module.ts
@@ -1,25 +1,50 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import {AppComponent}  from './app.component';
 import {FormsModule} from "@angular/forms";
 import {HeroDetailComponent} from "./hero-detail.component";
 import {HeroService} from "./hero.service";
+import {HeroesComponent} from "./heroes.component";
+import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./bashboard.component";
+
 
 @NgModule({
-  imports:[
+  imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+
+    ])
   ],
-  declarations:[
+  declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
-  bootstrap:[
+  bootstrap: [
     AppComponent
   ],
-  providers:[
+  providers: [
     HeroService
   ]
 })
-export class AppModule { }
+
+
+export class AppModule {
+}
