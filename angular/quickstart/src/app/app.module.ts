9044410ca9f50b3eb2f35bd915380bb787cd0a8b
Reza Shokri <bioharz3@gmail.com>
2017-06-05 18:35:17 +0200

    local fun with angular heros

--- angular/quickstart/src/app/app.module.ts
+++ angular/quickstart/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:[
+    BrowserModule,
+    FormsModule
+  ],
+  declarations:[
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap:[
+    AppComponent
+  ],
+  providers:[
+    HeroService
+  ]
 })
 export class AppModule { }
