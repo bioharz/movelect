ed9a1a87ee4cdb0fbf93f359ed858ce6b91bec84
Reza Shokri <bioharz3@gmail.com>
2017-06-23 16:43:03 +0200

    create db config tempalte

--- mvc-base/includes/dbconfig.php
+++ mvc-base/includes/dbconfig.php
@@ -3,6 +3,8 @@
 define('DB_HOST', 'localhost');
 //define('DB_NAME', 'mvc');
 define('DB_NAME', 'movelect');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_USER', 'movelect');
+//define('DB_USER', 'root');
+//define('DB_PASS', 'root');
+define('DB_PASS', 'K;G8;nSCKz\fljZV');
 
