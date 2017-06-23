88c80d69cedc87564877be2ffa71119dd6b746c1
Reza Shokri <bioharz3@gmail.com>
2017-06-23 19:47:30 +0200

    change error reporting

--- mvc-base/includes/error_reporting.php
+++ mvc-base/includes/error_reporting.php
@@ -1,4 +1,12 @@
 <?php
-//error_reporting(E_ALL);
-//ini_set("display_errors", 1);
-ini_set("display_errors", 0);
+
+$showErros = false; //switch this to true for error reporting
+
+if($showErros) {
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+} else {
+    error_reporting(0);
+    ini_set("display_errors", 0);
+}
