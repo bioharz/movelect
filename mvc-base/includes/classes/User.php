02d847b3bd526cc4d8f4063a91e64ff40fb5a8d9
Reza Shokri <bioharz3@gmail.com>
2017-06-23 15:12:03 +0200

    change models and controller. move email validator from model to controller

--- mvc-base/includes/classes/User.php
+++ mvc-base/includes/classes/User.php
@@ -9,82 +9,77 @@ class User extends Database
 
     //TODO return true or false by certain statements(like delete, create....)
 
-	public $username = '';
-	public $id = '';
+    public $username = '';
+    public $id = '';
 
-	public $isLoggedIn = false;
+    public $isLoggedIn = false;
 
-	/**
-	 * User constructor.
-	 * This is a little crazy - especailly the "fillIt" and "shipIt" part.
-	 * Instead of just saving a normal value like an integer or a string
-	 * one is able to save complex structures by serializing them and store them as a string
-	 * with that method - we are able to save public attributes in the session
-	 * if there are values in the session we fill our object with those values
-	 * not magic - but a little complex on the first sight
-	 */
-	public function __construct()
-	{
-		parent::__construct();
+    /**
+     * User constructor.
+     * This is a little crazy - especailly the "fillIt" and "shipIt" part.
+     * Instead of just saving a normal value like an integer or a string
+     * one is able to save complex structures by serializing them and store them as a string
+     * with that method - we are able to save public attributes in the session
+     * if there are values in the session we fill our object with those values
+     * not magic - but a little complex on the first sight
+     */
+    public function __construct()
+    {
+        parent::__construct();
 
-		if($_SESSION[get_class($this).'Ship'] != '')
-		{
-			$ship = $_SESSION[get_class($this)."Ship"];
-			$this->fillIt($ship);
-		}
-	}
+        if ($_SESSION[get_class($this) . 'Ship'] != '') {
+            $ship = $_SESSION[get_class($this) . "Ship"];
+            $this->fillIt($ship);
+        }
+    }
 
-	/**
-	 * save our values in the session
-	 */
-	public function __destruct()
-	{
-		parent::__destruct();
+    /**
+     * save our values in the session
+     */
+    public function __destruct()
+    {
+        parent::__destruct();
 
-		$_SESSION[get_class($this).'Ship'] = $this->shipIt();
-	}
+        $_SESSION[get_class($this) . 'Ship'] = $this->shipIt();
+    }
 
-	/**
-	 * Checks if the User is logged in - if not redirect him to the login page
-	 * @return bool
-	 */
-	public function authenticate()
-	{
-		//checks if the user is logged in - if not - redirect to login!
-		if(!$this->isLoggedIn)
-		{
-			define('LOGGED_IN', false);
+    /**
+     * Checks if the User is logged in - if not redirect him to the login page
+     * @return bool
+     */
+    public function authenticate()
+    {
+        //checks if the user is logged in - if not - redirect to login!
+        if (!$this->isLoggedIn) {
+            define('LOGGED_IN', false);
 
-			$this->redirectToLogin();
+            $this->redirectToLogin();
 
-			//return false;
-		}
+            //return false;
+        }
 
-		define('LOGGED_IN', true);
+        define('LOGGED_IN', true);
 
-		return true;
-	}
+        return true;
+    }
 
-	public function redirectToLogin()
-	{
-		if(API_CALL === true)
-		{
-			header('Location: ../'.LOGIN_URL);
-		}
-		else
-		{
-			header('Location: '.LOGIN_URL);
-		}
-		header('Status: 303');
-		exit();
-	}
+    public function redirectToLogin()
+    {
+        if (API_CALL === true) {
+            header('Location: ../' . LOGIN_URL);
+        } else {
+            header('Location: ' . LOGIN_URL);
+        }
+        header('Status: 303');
+        exit();
+    }
 
-	public function redirectToIndex()
-	{
-		header('Location: '.INDEX_URL);
-		header('Status: 303');
-		exit();
-	}
+    public function redirectToIndex()
+    {
+        header('Location: ' . INDEX_URL);
+        header('Status: 303');
+        exit();
+    }
 
     /**
      * @param $email
@@ -92,160 +87,164 @@ class User extends Database
      * @return bool
      */
     public function login($email, $pass)
-	{
-		$sql = "SELECT `id`,`pass` FROM `user` WHERE `email`='" . $this->escapeString($email) . "'";
-		$result = $this->query($sql);
+    {
+        $sql = "SELECT `id`,`pass` FROM `user` WHERE `email`='" . $this->escapeString($email) . "'";
+        $result = $this->query($sql);
 
 
-		if($this->numRows($result) == 0)
-		{
-			$this->isLoggedIn = false;
-			return false; //username not found!
-		}
+        if ($this->numRows($result) == 0) {
+            $this->isLoggedIn = false;
+            return false; //username not found!
+        }
 
-		//now lets check for the password
-		$row = $this->fetchObject($result);
+        //now lets check for the password
+        $row = $this->fetchObject($result);
 
-		if(password_verify($pass, $row->pass))
-		{
-			$this->email = $email;
-			$this->id = $row->id;
-			$this->isLoggedIn = true;
+        if (password_verify($pass, $row->pass)) {
+            $this->email = $email;
+            $this->id = $row->id;
+            $this->isLoggedIn = true;
 
-			return true;
-		}
+            return true;
+        }
 
-		$this->isLoggedIn = false;
-		return false;
-	}
+        $this->isLoggedIn = false;
+        return false;
+    }
 
-	public static function getById($id)
-	{
-		$id = intval($id);
-		$sql = "SELECT * FROM `user` WHERE `id`=".$id;
+    public static function getById($id)
+    {
+        $id = intval($id);
+        $sql = "SELECT * FROM `user` WHERE `id`=" . $id;
 
-		$db = new Database();
-		$result = $db->query($sql);
+        $db = new Database();
+        $result = $db->query($sql);
 
-		if($db->numRows($result) > 0)
-		{
-			//get the data
-			$data = $db->fetchObject($result);
-			$user = new User();
+        if ($db->numRows($result) > 0) {
+            //get the data
+            $data = $db->fetchObject($result);
+            $user = new User();
 
-			$user->username = $data['username'];
-			$user->id = $id;
+            $user->username = $data['username'];
+            $user->id = $id;
 
-			return $user;
-		}
+            return $user;
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	public function logout()
-	{
-		$this->username = null;
-		$this->id = null;
-		$this->isLoggedIn = false;
-		$this->shipIt();
+    public function logout()
+    {
+        $this->username = null;
+        $this->id = null;
+        $this->isLoggedIn = false;
+        $this->shipIt();
 
-		//$this->redirectToLogin();
+        //$this->redirectToLogin();
 
-		return true;
-	}
-
-	/**
-	 * Gets all attributes from this class, serializes it adds slahes to save this string in the session
-	 * @return string
-	 */
-	protected function shipIt()
-	{
-		$ship = serialize($this);
-		$ship = addslashes($ship);
-		return $ship;
-	}
-
-	/**
-	 * Fills this class with the data from the session which was previously saved
-	 * @param $ship
-	 */
-	protected function fillIt($ship)
-	{
-		$ship = stripslashes($ship);
-		$thiz = unserialize($ship);
-		$ro = new reflectionObject($thiz);
-		foreach ($ro->getProperties() as $propObj)
-		{
-			$this->{$propObj->name} = $thiz->{$propObj->name};
-		}
-	}
-
-	public static function existsWithUsername($username)
-	{
-		$db = new Database();
-
-		//check if user exists...
-		$sql = "SELECT COUNT(`id`) AS num FROM `user` WHERE `name`='".$db->escapeString($username)."'";
-		$result = $db->query($sql);
-
-		$row = $db->fetchObject($result);
-
-		if($row->num == 0)
-		{
-			return false;
-		}
-
-		return true;
-	}
+        return true;
+    }
 
     /**
-     * @param $data[name,email,since,pass]
+     * Gets all attributes from this class, serializes it adds slahes to save this string in the session
+     * @return string
+     */
+    protected function shipIt()
+    {
+        $ship = serialize($this);
+        $ship = addslashes($ship);
+        return $ship;
+    }
+
+    /**
+     * Fills this class with the data from the session which was previously saved
+     * @param $ship
+     */
+    protected function fillIt($ship)
+    {
+        $ship = stripslashes($ship);
+        $thiz = unserialize($ship);
+        $ro = new reflectionObject($thiz);
+        foreach ($ro->getProperties() as $propObj) {
+            $this->{$propObj->name} = $thiz->{$propObj->name};
+        }
+    }
+
+    public static function existsWithUsername($username)
+    {
+        $db = new Database();
+
+        //check if user exists...
+        $sql = "SELECT COUNT(`id`) AS num FROM `user` WHERE `name`='" . $db->escapeString($username) . "'";
+        $result = $db->query($sql);
+
+        $row = $db->fetchObject($result);
+
+        if ($row->num == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    public static function existsWithEmail($email)
+    {
+        $db = new Database();
+
+        //check if email exists...
+        $sql = "SELECT COUNT(`id`) AS num FROM `user` WHERE `email`='" . $db->escapeString($email) . "'";
+        $result = $db->query($sql);
+
+        $row = $db->fetchObject($result);
+
+        if ($row->num == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $data [name,email,since,pass]
      * @return bool
      */
     public static function createUser($data)
-	{
-		$db = new Database();
+    {
+        $db = new Database();
 
-		$name = $db->escapeString($data['name']);
-		$pass = password_hash($db->escapeString($data['pass']), PASSWORD_BCRYP);
+        $name = $db->escapeString($data['name']);
+        $pass = password_hash($db->escapeString($data['pass']), PASSWORD_BCRYP);
 
-		//$email = '';
+        $email = $db->escapeString($data['email']);
 
-		if(Utils::isValidEmail( $data['email'])) {
-            $email = $db->escapeString($data['email']);
-
-        } else {
-            trigger_error("Invalid Email!!!", E_USER_ERROR);
-		    return false;
-        }
-
-		$sql = "INSERT INTO `user`(`name`,email,since,`pass`) VALUES('".$name."','".$email."','".$data[since]."','".$pass."')";
-		$db->query($sql);
-
-		//return true????
-	}
-
-	public static function deleteUser($id)
-	{
-
-		$db = new Database();
-
-		$sql ="DELETE FROM user WHERE id=".intval($id);
+        $sql = "INSERT INTO `user`(`name`,email,since,`pass`) VALUES('" . $name . "','" . $email . "','" . $data[since] . "','" . $pass . "')";
         $db->query($sql);
-	}
 
-	public static function updateUser($data)
-	{
-		//@TODO
-	}
+    }
 
-	public function delete()
-	{
-		self::deleteUser($this->id);
-	}
+    public static function deleteUser($id)
+    {
 
-	public function update($data)
-	{
-		self::updateUser($this->id, $data);
-	}
+        $db = new Database();
+
+        $sql = "DELETE FROM user WHERE id=" . intval($id);
+        $db->query($sql);
+    }
+
+    public static function updateUser($data)
+    {
+        //@TODO
+    }
+
+    public function delete()
+    {
+        self::deleteUser($this->id);
+    }
+
+    public function update($data)
+    {
+        self::updateUser($this->id, $data);
+    }
 }
