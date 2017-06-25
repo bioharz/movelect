0e447091861ec0ce789de3647e2628e4d3313b9a
Reza Shokri <bioharz3@gmail.com>
2017-06-25 23:26:47 +0200

    implement imdb search in mvc

--- baseproject/includes/views/movie.php
+++ baseproject/includes/views/movie.php
@@ -1,4 +1,31 @@
 <div class="row">
+
+    <div class="row date">
+        <div class="col-xs-4"></div>
+        <div class="col-xs-4">
+            <input type="text" class="form-control text-center" id="inputMovie" placeholder="add movie">
+            <button id="searchMovie" type="submit" class="col-xs-4 btn btn-default">search</button>
+            <table class="table table-striped" id="results">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Year</th>
+                    <th>Poster</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </tr>
+                </tbody>
+
+            </table>
+        </div>
+        <div class="col-xs-4"></div>
+    </div>
+
     <form method="<?php if($this->id): ?>put<?php else: ?>post<?php endif; ?>" action="api/movie/" class="col-xs-12">
 
         <div class="form-group">
@@ -28,6 +55,72 @@
 </div>
 <script type="text/javascript">
 
+    var selectedMovie;
+
+
+    (function () {
+        $(init);
+        function init() {
+
+            var apiKey = "apikey=a7d2828f";
+
+            $("#searchMovie").click(searchMovie);
+            var inputMovie = $("#inputMovie");
+            var table = $("#results");
+            var tbody = table.find("tbody");
+
+            function searchMovie() {
+                var title = inputMovie.val();
+
+                $.ajax({
+                    url: "http://www.omdbapi.com/?s=" + title + "&" + apiKey,
+                    //dataType: "jsonp",
+                    success: renderMovies
+                });
+            }
+
+            function renderMovies(movies) {
+                tbody.empty();
+
+                /*
+                 returned Json:
+                 "Search": [
+                 {
+                 "Title": "Star Wars: Episode IV - A New Hope",
+                 "Year": "1977",
+                 "imdbID": "tt0076759",
+                 "Type": "movie",
+                 "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
+                 }
+                 */
+
+                var result = movies["Search"];
+
+                for (var counter in result) {
+
+                    var movie = result[counter];
+
+                    var tr = $("<tr>");
+                    var titletd = $("<td>").append(movie.Title);
+                    var yeartd = $("<td>").append(movie.Year);
+                    var postertd = $("<td>").append("<img src=\"" + movie.Poster + "\"  width='100'  onclick=\"selectMovie(movie.imdbID)\" > ");
+
+                    tr.append(titletd);
+                    tr.append(yeartd);
+                    tr.append(postertd);
+                    tbody.append(tr);
+
+                }
+
+            }
+
+            function selectMovie(movie) {
+
+                console.log(movie);
+
+            }
+        }
+    })();
 
 
 	var editModal = $('#editModal');
--- public_html/imdb.js
+++ public_html/imdb.js
@@ -1,82 +1,56 @@
-(function(){
+(function () {
     $(init);
-    function init()
-    {
+    function init() {
 
         var apiKey = "apikey=a7d2828f";
-
 
         $("#searchMovie").click(searchMovie);
         var inputMovie = $("#inputMovie");
         var table = $("#results");
         var tbody = table.find("tbody");
-        function searchMovie()
-        {
+
+        function searchMovie() {
             var title = inputMovie.val();
 
             $.ajax({
-                url:"http://www.omdbapi.com/?s="+title+"&"+apiKey,
+                url: "http://www.omdbapi.com/?s=" + title + "&" + apiKey,
                 //dataType: "jsonp",
                 success: renderMovies
             });
         }
-        function renderMovies(movies){
-            //console.log(movies);
+
+        function renderMovies(movies) {
             tbody.empty();
 
-/*
- "Search": [
- {
- "Title": "Star Wars: Episode IV - A New Hope",
- "Year": "1977",
- "imdbID": "tt0076759",
- "Type": "movie",
- "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
- }
- */
+            /*
+            returned Json:
+             "Search": [
+             {
+             "Title": "Star Wars: Episode IV - A New Hope",
+             "Year": "1977",
+             "imdbID": "tt0076759",
+             "Type": "movie",
+             "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BYzQ2OTk4N2QtOGQwNy00MmI3LWEwNmEtOTk0OTY3NDk2MGJkL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
+             }
+             */
 
-var result = movies["Search"];
+            var result = movies["Search"];
 
-for(var counter in result) {
+            for (var counter in result) {
 
-    var movie = result[counter];
-
-    var tr = $("<tr>");
-    var titletd = $("<td>").append(movie.Title);
-    var yeartd = $("<td>").append(movie.Year);
-
-
-    var postertd = $("<td>").append("<img src=\""+movie.Poster+"\">");
-
-
-
-    tr.append(titletd);
-    tr.append(yeartd);
-    tr.append(postertd);
-    tbody.append(tr);
-
-
-
-}
-
-/*
-                var title2 = movie.Title;
-                var year = movie.Year;
-               // var plot = movie.Plot;
-                var poster = "<img src=\""+movie.Poster+"\">";
-              //  var actors = movie.Actors;
-               // var director = movie.Director;
+                var movie = result[counter];
 
                 var tr = $("<tr>");
-                var titletd = $("<td>").append(title2);
-                var yeartd = $("<td>").append(year);
-                var postertd = $("<td>").append(poster);
+                var titletd = $("<td>").append(movie.Title);
+                var yeartd = $("<td>").append(movie.Year);
+                var postertd = $("<td>").append("<img src=\"" + movie.Poster + "\">");
+
                 tr.append(titletd);
                 tr.append(yeartd);
                 tr.append(postertd);
                 tbody.append(tr);
 
-*/
+            }
 
         }
     }
