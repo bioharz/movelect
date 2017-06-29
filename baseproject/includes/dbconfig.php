79cbbf72fc5eeb51c5396eaa20efefc1de567325
Daniel <daniel.whitehead1993@gmail.com>
2017-06-30 00:29:19 +0200

    design fixes, look at fixlog for more info

--- baseproject/includes/dbconfig.php
+++ baseproject/includes/dbconfig.php
@@ -6,5 +6,5 @@ define('DB_NAME', 'mvc');
 //define('DB_USER', 'movelect');
 //define('DB_PASS', 'K;G8;nSCKz\fljZV');
 
-define('DB_USER', 'mvc');
-define('DB_PASS', 'mvc');
+define('DB_USER', 'root');
+define('DB_PASS', '');
